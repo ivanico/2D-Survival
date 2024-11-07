76e891b6442ae0eb61a3999725f2b80bb47fb1ab
ivanico <ivan.petrevski3@gmail.com>
2024-11-07 20:14:51 +0100

    added ability upgrade functionality

--- scenes/autoload/game_events.gd
+++ scenes/autoload/game_events.gd
@@ -2,7 +2,11 @@ extends Node
 
 
 signal exp_vial_collected(number: float)
-
+signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary)
 
 func emit_exp_vial_collected(number: float):
 	exp_vial_collected.emit(number)
+
+
+func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	ability_upgrade_added.emit(upgrade, current_upgrades)
