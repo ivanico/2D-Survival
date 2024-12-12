d55115bbecb8dfd88d40b0a3ccb7d1252c74fa7c
ivanico <ivan.petrevski3@gmail.com>
2024-12-12 03:53:11 +0100

    animating upgrade cards and defeat screen

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -4,15 +4,51 @@ signal selected
 
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
+@onready var animation_player = $AnimationPlayer
+@onready var hover_animation_player = $HoverAnimationPlayer
+
+var disabled = false
 
 func _ready():
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_enterd)
 	
-	
+
+func play_in(delay: float = 0):
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	animation_player.play("in")
+
+
+func play_discard():
+	animation_player.play("discard")
+
+
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
 
+
+func select_card():
+	disabled = true
+	animation_player.play("selected")
+	
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card == self:
+			continue
+		other_card.play_discard()
+	
+	await animation_player.animation_finished
+	selected.emit()
+
 func on_gui_input(event: InputEvent):
+	if disabled:
+		return
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+
+
+func on_mouse_enterd():
+	if disabled:
+		return
+	hover_animation_player.play("hover")
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -4,21 +4,26 @@ signal upgrade_selected(upgrade: AbilityUpgrade)
 
 @export var upgrade_card_scene: PackedScene
 
+@onready var animation_player = $AnimationPlayer
 @onready var card_container: HBoxContainer = %CardContainer
 
 func _ready():
 	get_tree().paused = true
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+	var delay = 0
 	for upgrade in upgrades:
 		var card_instance = upgrade_card_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade) #the function from ability_upgrade_card
-		card_instance.selected.connect(on_upgrade_selected.bind(upgrade)) #here we have selected in ability_upg_card as signal and we call it here
-																		  #and when it's emited we bind the upgrade that we clicked on so we get the values (name and description)
+		card_instance.play_in(delay)
+		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))  #here we have selected in ability_upg_card as signal and we call it here
+		delay += 0.2															#and when it's emited we bind the upgrade that we clicked on so we get the values (name and description)
 
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	upgrade_selected.emit(upgrade)
+	animation_player.play("out")
+	await animation_player.animation_finished
 	get_tree().paused = false
 	queue_free()
